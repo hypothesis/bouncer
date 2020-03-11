eeec80d548c48a9832b5402059d8c288164c8ccf
Sean Hammond <git@snhmnd.fastmail.com>
2020-03-11 15:53:12 +0000

    Add our standard `patch` fixture
    Just copy-pasted from LMS. Nothing in bouncer uses this yet but it will
    doubtless come in useful

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import functools
+from unittest import mock
+
+import pytest
+
+
+def autopatcher(request, target, **kwargs):
+    """Patch and cleanup automatically. Wraps :py:func:`mock.patch`."""
+    options = {"autospec": True}
+    options.update(kwargs)
+    patcher = mock.patch(target, **options)
+    obj = patcher.start()
+    request.addfinalizer(patcher.stop)
+    return obj
+
+
+@pytest.fixture
+def patch(request):
+    return functools.partial(autopatcher, request)
